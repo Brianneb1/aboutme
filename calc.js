fc90a1168c86ec61595ec6ec00c932ce8a7b54dd
Brianneb1 <42755820+Brianneb1@users.noreply.github.com>
2019-02-19 15:07:34 -0600

    implemented math facts API

--- calc.js
+++ calc.js
@@ -7,6 +7,7 @@ const radiusElement = document.getElementById("radius");
 const heightElement = document.getElementById("height");
 const btnElement = document.getElementById("calcbtn");
 const answerElement = document.getElementById("answerText");
+const mathElement = document.getElementById("math");
 
 const radius = radiusElement.value;
 const height = heightElement.value;
@@ -21,6 +22,7 @@ document.getElementById("height").innerHTML = parseFloat(storedHeight);
 console.log('INFO: Configuring event handlers');
 
 btnElement.addEventListener('click', function(){calcVolume(radiusElement.value,heightElement.value)});
+mathElement.addEventListener('dblclick', function(){mathHandler()});
 
 console.log('INFO: Declaring functions');
 
@@ -38,10 +40,44 @@ function calcVolume(r,h){
         answerElement.innerHTML = `Invalid input - cannot be null`
         return null;
     }
+    else if(r > 100000 || h > 100000){
+        answerElement.innerHTML = `Invalid input - input too large`
+        return null;
+    }
     else{
         answerElement.innerHTML = `The volume is: ${volume} cubic centimeters.`
         return volume;
     }
 }
 
+//based off Professor Case's example in the slides
+function getMathFact() {
+    return new Promise(function (resolve, reject) {  
+      const req = new XMLHttpRequest();  
+      req.timeout = 2000; 
+      req.onreadystatechange = function (e) {
+        if (req.readyState === 4) {
+          if (req.status === 200) {
+            const fact = req.response;
+            resolve(fact)
+          } else {
+            reject(req.status)
+          }
+        }
+      }
+      req.ontimeout = function () {
+        reject('Error - timed out: ' + req.time)
+      }
+      req.open("GET", "http://numbersapi.com/random/math", true);  
+      req.send();
+    })
+  }
+
+  async function mathHandler() {
+    const fact = await getMathFact();
+    console.log(fact);
+    mathElement.innerHTML = fact;
+  }
+
 console.log('INFO: Done loading, waiting for an event'); 
+ 
